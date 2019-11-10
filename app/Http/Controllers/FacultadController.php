e4f3d7e3b8ecf022cdb1b87b503c0ac0ed1422fa
jmurilloariza <jmurilloariza@gmail.com>
2019-11-10 13:21:50 -0500

    correcion eliminar fcultad

--- app/Http/Controllers/FacultadController.php
+++ app/Http/Controllers/FacultadController.php
@@ -157,9 +157,8 @@ class FacultadController extends Controller
         $facultad = Facultad::find($id);
 
         $relaciones = $facultad->with(['departamentos.programasAcademicos'])->get()->toArray()[0];
-
-        for ($i = 0, $long = count($relaciones['departamentos']); $i < $long; $i++) { 
-            $departamento = $relaciones[$i];
+        for ($i = 0, $long = count($relaciones['departamentos']); $i < $long; $i++) {
+            $departamento = $relaciones['departamentos'][$i];
             if(count($departamento['programas_academicos']) > 0){
                 ProgramaAcademico::where(['departamento_id' => $departamento['id']])->update(['codigo' => null]);
             }
